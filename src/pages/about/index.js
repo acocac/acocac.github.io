62eb67310042c0f05138d36616edde76dbecce28
Alejandro © <acocac@gmail.com>
2022-06-25 23:42:26 +0100

    add translate to about

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Translate, { translate } from '@docusaurus/Translate';
 import clsx from "clsx";
 
 import Link from "@docusaurus/Link";
@@ -22,37 +23,73 @@ function About() {
                 <img src={useBaseUrl("img/me.jpeg")} />
               </div>
               <div className="col col--9">
-                <h2>Hola! &#9961;</h2>
+                <h2>
+                  <Translate>
+                    Hola! &#9961;
+                  </Translate>
+                </h2>
                 <p>
-                  Soy Alejandro, experto en ciencias de datos y geotecnologías principalmente para estudios medioambientales y aplicaciones en ordenamiento territorial con ubicación actual en Londres, Reino Unido.
+                  <Translate>
+                    Soy Alejandro, experto en ciencias de datos y geotecnologías principalmente para estudios medioambientales y aplicaciones en ordenamiento territorial con ubicación actual en Londres, Reino Unido.
+                  </Translate>
                 </p>
                 <p>
-                  Me apasiona los desarrollos tecnológicos, en especial aquellos que tienen impacto social. He participado en una variedad de{" "}
+                  <Translate>
+                    Me apasiona los desarrollos tecnológicos, en especial aquellos que tienen impacto social. He participado en una variedad de
+                  </Translate>{" "}
                   <Link to={useBaseUrl("projects/")}>
-                    proyectos multidisciplinarios{" "}
-                  </Link>
+                    <Translate>
+                      proyectos multidisciplinarios
+                    </Translate>
+                  </Link>{" "}
+                  <Translate>
                    apoyados en el análisis de datos. En mis tiempos libres disfruto de mi familia &#128106;, senderismo &#127956;, ciclismo &#128692;, y probar variedades de Café (como buen representante de mi pais, Colombia 🇨🇴).
+                  </Translate>
                 </p>
                 <p>
-                  Cuento con un doctorado en Geografía Física y maestría en Modelamiento, Monitoreo y Manejo Ambiental {" "}
-                  de{" "}
+                  <Translate>
+                    Cuento con un doctorado en Geografía Física y maestría en Modelamiento, Monitoreo y Manejo Ambiental de
+                  </Translate>{" "}
                   <a href="https://www.kcl.ac.uk/geography/">
                     King's College London
-                  </a>. En colaboración con investigadores de la{" "}
+                  </a>
+                  <Translate>
+                    . En colaboración con investigadores de la
+                  </Translate>{" "}
                   <a href="https://alliancebioversityciat.org/">
-                    Alianza Bioversity-CIAT
-                  </a>, en mis estudios de posgrado desarrollé bases sólidas en ciencias de datos y computación de grandes datos en la nube para proponer metodologías de caracterización espacial y temporal de los motores de la deforestación en el bioma Amazónico a partir de productos satelitales y capas geoespaciales.
+                    <Translate>
+                      Alianza Bioversity-CIAT
+                    </Translate>
+                  </a>{" "}
+                    <Translate>
+                      , en mis estudios de posgrado desarrollé bases sólidas en ciencias de datos y computación de grandes datos en la nube para proponer metodologías de caracterización espacial y temporal de los motores de la deforestación en el bioma Amazónico a partir de productos satelitales y capas geoespaciales.
+                    </Translate>
                 </p>
                 <p>
-                  Estoy afiliado a{" "}
-                  <a href="https://www.turing.ac.uk/">The Alan Turing Institute</a> como investigador Postdoctoral asociado. Gracias al soporte de un equipo diverso de profesionales de mi{" "}
+                  <Translate>
+                    Estoy afiliado a
+                  </Translate>{" "}
+                  <a href="https://www.turing.ac.uk/">
+                    <Translate>
+                      The Alan Turing Institute
+                    </Translate>
+                    </a>{" "}
+                    <Translate>
+                      como investigador Postdoctoral asociado. Gracias al soporte de un equipo diverso de profesionales de mi
+                    </Translate>{" "}
                   <a href="https://www.turing.ac.uk/research/research-projects/environmental-monitoring-blending-satellite-and-surface-data/">
-                    proyecto
-                  </a>, el instituto y comunidades que hago parte, he podido enfocarme al desarrollo de programas y herramientas de código abierto para ciencias ambientales. Asimismo, me entreno en el liderazgo de ciencia abierta, compartiendo mis experiencias en mi blog y notas.
+                    <Translate>
+                      proyecto
+                    </Translate>
+                  </a>
+                    <Translate>
+                      , el instituto y comunidades que hago parte, he podido enfocarme al desarrollo de programas y herramientas de código abierto para ciencias ambientales. Asimismo, me entreno en el liderazgo de ciencia abierta, compartiendo mis experiencias en mi blog y notas.
+                    </Translate>
                 </p>
                 <p>
-                  Puedes leer mas de mis contribuciones, educación y charlas en mi Curriculum Vitae
-.
+                  <Translate>
+                    Puedes leer mas de mis contribuciones, educación y charlas en mi Curriculum Vitae.
+                  </Translate>
                 </p>
               </div>
             </div>
